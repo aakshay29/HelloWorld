2fc1a9f49e9526860ebb9abd1815fcf234997787
aakshay29 <aakshay.29@gmail.com>
2016-09-02 11:34:53 -0400

    Receipt Printing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,21 @@
-import java.util.Date;
-
 public class HelloWorld {
 	
 	public static void main (String[] args){
-		//first program
-		Date d = new Date();
-		System.out.println("Hello World!");
-		System.out.println("My name is Aakshay. Date :" + d);
-		System.out.println("I love Java");
+		System.out.println("+--------------------------------------+");
+		System.out.println("|      Java Bank ATM Receipt           |");
+		System.out.println("|      Wednesday, December 2, 2015     |");
+		System.out.println("|      ATM Location # 123              |");
+		System.out.println("|                                      |");
+		System.out.println("|                                      |");
+		System.out.println("|      Account Number:      1234567    |");
+		System.out.println("|      Customer:     John Q. Public    |");
+		System.out.println("|      Transaction Type:    Deposit    |");
+		System.out.println("|      Transaction Amount:  $500.00    |");
+		System.out.println("|      Account Balance:   $1,500.00    |");
+		System.out.println("|                                      |");
+		System.out.println("|      Thank you for banking with us   |");
+		System.out.println("|            Have a coffee day         |");
+		System.out.println("|                                      |");
+		System.out.println("+--------------------------------------+");
 	}
 }
