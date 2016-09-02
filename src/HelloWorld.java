135d83710dfe7f2b0c942d4eff9dc07c37e2ec0c
aakshay29 <aakshay.29@gmail.com>
2016-09-02 11:26:19 -0400

    NewLIne

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,10 @@ import java.util.Date;
 public class HelloWorld {
 	
 	public static void main (String[] args){
+		//first program
 		Date d = new Date();
 		System.out.println("Hello World!");
 		System.out.println("My name is Aakshay. Date :" + d);
+		System.out.println("I love Java");
 	}
 }
