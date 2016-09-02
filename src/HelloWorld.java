25cdab2f5d15626f1e0e84d1ac142b6001352920
oracle <oracle@GBTC441R02RH.montgomerycollege.edu>
2016-09-02 11:18:42 -0400

    first

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class HelloWorld {
+	
+	public static void main (String[] args){
+		Date d = new Date();
+		System.out.println("Hello World!");
+		System.out.println("My name is Aakshay. Date :" + d);
+	}
+}
